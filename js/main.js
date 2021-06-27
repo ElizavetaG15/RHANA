3083e9a1d6fed9634be243164717b9eaa6a79635
ElizavetaG15 <85743223+ElizavetaG15@users.noreply.github.com>
2021-06-27 19:22:23 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ $(function() {
 
     $('.banner__wrapper').slick({
 
+        slidesToShow: 1,
+
         prevArrow: '<button type="button" class="slick-btn slick-prev banner-prev"> <svg width="12" height="20" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg">' +
             '<path d="M10 18L2 10L10 2" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>' +
             '</svg>',
@@ -20,6 +22,32 @@ $(function() {
         prevArrow: '<button type="button" class="slick-btn slick-prev special-prev"> <img src="images/special-slick-prev.svg" alt=""> </button>',
         nextArrow: '<button type="button" class="slick-btn slick-next special-next"> <img src="images/special-slick-next.svg" alt=""> </button>',
 
+        responsive: [{
+                breakpoint: 1231,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+
+            {
+                breakpoint: 590,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false,
+                }
+            },
+
+
+        ]
+
+
     })
 });
 
@@ -30,6 +58,32 @@ $(function() {
 
         prevArrow: '<button type="button" class="slick-btn slick-prev special-prev"> <img src="images/special-slick-prev.svg" alt=""> </button>',
         nextArrow: '<button type="button" class="slick-btn slick-next special-next"> <img src="images/special-slick-next.svg" alt=""> </button>',
+
+        responsive: [{
+                breakpoint: 1230,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+
+                }
+            },
+
+            {
+                breakpoint: 590,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false,
+                }
+            },
+
+
+        ]
 
     })
 });
@@ -42,5 +96,63 @@ $(function() {
         prevArrow: '<button type="button" class="slick-btn slick-prev special-prev"> <img src="images/special-slick-prev.svg" alt=""> </button>',
         nextArrow: '<button type="button" class="slick-btn slick-next special-next"> <img src="images/special-slick-next.svg" alt=""> </button>',
 
+        responsive: [{
+                breakpoint: 1230,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 930,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+
+            {
+                breakpoint: 730,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false,
+                }
+            },
+        ]
+
+
     })
+});
+
+$(function() {
+    $('.plarecanta__mobile-slider').slick({
+            slidesToShow: 2,
+            arrows: false,
+        }
+
+    );
+});
+
+$(function() {
+    $('.bblaboratories__mobile-slider').slick({
+            slidesToShow: 2,
+            arrows: false,
+        }
+
+    );
+});
+
+// $('.menu-burger').click(function(e) {
+//     e.preventDefault();
+//     $('.overlay').fadeIn(800);
+// });
+
+// $('.mobile-close-icon').click(function() {
+//     $('.overlay').fadeOut(800);
+// });
+
+$('.menu-burger').on('click', function() {
+    $('.overlay').fadeIn(800);
+});
+
+$('.mobile-close-icon').on('click', function() {
+    $('.overlay').fadeOut(800);
 });
